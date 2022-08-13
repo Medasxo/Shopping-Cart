fdf38e9defee019b00aba285be7151d510476c91
Medas <medas02345@gmail.com>
2022-08-13 19:32:52 +0300

    Add a homepage styling and structure

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,16 @@
-
-import '../App.css';
+import "../App.css";
 import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <div className="mainHome">
+        <h2>Porsche</h2>
+        <h4>There Is No Substitute</h4>
+        <button type="button" className="shopButton">SHOP</button>
+      </div>
+      
     </div>
   );
 }
