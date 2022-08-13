bc80ac641a95056f40e3276d03e7b0b373dc17a0
Medas <medas02345@gmail.com>
2022-08-13 16:34:28 +0300

    Add a link(routing) to Home Page when clicking on Porsche and a link to /shop when clicking on Shop

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,22 @@
 import porscheLogo from "../img/porscheLogo.png";
 import cartIcon from "../img/cartIcon.png";
+import { Link } from "react-router-dom";
 import "../Header.css";
-
 
 function Header() {
   return (
     <nav className="header">
       <div className="porsche">
         <img src={porscheLogo} alt="porsche logo" />
-        Porsche
+        <Link to = '/'>
+        <p>Porsche</p>
+        </Link>
+        
       </div>
       <div className="links">
-        Shop
+        <Link to = '/shop'>
+        <p>Shop</p>
+        </Link>
         <img src={cartIcon} alt="cart icon" />
       </div>
     </nav>
---
+++ src/Components/Shop.js
@@ -0,0 +1,13 @@
+
+
+import Header from "./Header";
+
+function Shop() {
+  return (
+    <div className="shop">
+      <Header />
+    </div>
+  );
+}
+
+export default Shop;
