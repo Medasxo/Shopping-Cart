f92c8e5182293e36d7398a15703ebd81f899efe4
Medas <medas02345@gmail.com>
2022-08-14 21:47:44 +0300

    Modify css in shop on how to display cars, add picture variable to the car component

--- src/Components/Car.js
+++ src/Components/Car.js
@@ -3,6 +3,7 @@ import React from "react";
 const Car = (props) => {
   return (
     <div className="car">
+        <img src={props.img} alt={props.name} />
       <h3>{props.name}</h3>
     </div>
   );
--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,24 +1,24 @@
-
 import "../Shop.css";
 import Header from "./Header";
 import Car from "./Car";
 
+import Boxster from "../img/cars/718boxster.png";
+import Taycan from "../img/cars/taycan.png";
+
 function Shop() {
   const cars = [
-    { name: "Porsche 718 Boxster"},
-    { name: "Porsche Taycan"},
-  ]
+    { name: "Porsche 718 Boxster", img: Boxster },
+    { name: "Porsche Taycan", img: Taycan },
+  ];
   return (
     <div className="shop">
       <Header />
       <div className="shopContent">
         <div className="shopGrid">
-        {cars.map((car) => {
-          return(
-          <Car name={car.name} />
-          );
-        })}
-          </div>
+          {cars.map((car) => {
+            return <Car name={car.name} img={car.img} />;
+          })}
+        </div>
       </div>
     </div>
   );
