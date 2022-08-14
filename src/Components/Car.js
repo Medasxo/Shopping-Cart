c45966cb3f4a90952c28753d1e24cf775aad1806
Medas <medas02345@gmail.com>
2022-08-14 21:29:59 +0300

    Add a Car component which will render all the cars to the grid in the shop

---
+++ src/Components/Car.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Car = (props) => {
+  return (
+    <div className="car">
+      <h3>{props.name}</h3>
+    </div>
+  );
+};
+
+export default Car;
--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,11 +1,25 @@
 
-
+import "../Shop.css";
 import Header from "./Header";
+import Car from "./Car";
 
 function Shop() {
+  const cars = [
+    { name: "Porsche 718 Boxster"},
+    { name: "Porsche Taycan"},
+  ]
   return (
     <div className="shop">
       <Header />
+      <div className="shopContent">
+        <div className="shopGrid">
+        {cars.map((car) => {
+          return(
+          <Car name={car.name} />
+          );
+        })}
+          </div>
+      </div>
     </div>
   );
 }
