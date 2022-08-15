1c6510d0203e949b4f2843ac32e5fa6a7c0d4c26
Medas <medas02345@gmail.com>
2022-08-15 10:30:39 +0300

    Add a on hover effect to the images, to display another picture

--- src/Components/Car.js
+++ src/Components/Car.js
@@ -3,10 +3,16 @@ import React from "react";
 const Car = (props) => {
   return (
     <div className="car">
-        <img src={props.img} alt={props.name} />
+      <img
+        src={props.img}
+        alt={props.name}
+        onMouseOver={(e) => (e.currentTarget.src = props.img2)}
+        onMouseOut={(e) => (e.currentTarget.src = props.img)}
+      />
       <h3>{props.name}</h3>
     </div>
   );
 };
+
 
 export default Car;
--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -3,12 +3,15 @@ import Header from "./Header";
 import Car from "./Car";
 
 import Boxster from "../img/cars/718boxster.png";
+import Boxster2 from "../img/cars/718boxster2.jpg";
 import Taycan from "../img/cars/taycan.png";
+import Taycan2 from "../img/cars/taycan2.jpg";
+
 
 function Shop() {
   const cars = [
-    { name: "Porsche 718 Boxster", img: Boxster },
-    { name: "Porsche Taycan", img: Taycan },
+    { name: "Porsche 718 Boxster", img: Boxster, img2: Boxster2},
+    { name: "Porsche Taycan", img: Taycan, img2: Taycan2 },
   ];
   return (
     <div className="shop">
@@ -16,7 +19,7 @@ function Shop() {
       <div className="shopContent">
         <div className="shopGrid">
           {cars.map((car) => {
-            return <Car name={car.name} img={car.img} />;
+            return <Car name={car.name} img={car.img} img2={car.img2}/>;
           })}
         </div>
       </div>
